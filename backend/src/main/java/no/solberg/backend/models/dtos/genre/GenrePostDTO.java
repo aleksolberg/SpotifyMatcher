f1e2569224d1e4a50b88c21222e3e815a3c3f247
Aleksander <aleks.solberg@gmail.com>
2023-09-21 22:03:48 +0200

    Implemented initial endpoints for Genre

---
+++ backend/src/main/java/no/solberg/backend/models/dtos/genre/GenrePostDTO.java
@@ -0,0 +1,10 @@
+package no.solberg.backend.models.dtos.genre;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class GenrePostDTO {
+    private String genreName;
+}
--- backend/src/main/java/no/solberg/backend/models/dtos/spotifyUser/SpotifyUserGetDTO.java
+++ backend/src/main/java/no/solberg/backend/models/dtos/spotifyUser/SpotifyUserGetDTO.java
@@ -8,6 +8,7 @@ import java.util.Set;
 @Getter
 @Setter
 public class SpotifyUserGetDTO {
+    private int id;
     private String name;
     private String email;
     private String accessToken;
